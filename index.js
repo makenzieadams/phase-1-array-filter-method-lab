f3b792fe3e6eb26d6be6b4aebbd10206740f68a2
Makenzie Adams <adamsm.realtor@gmail.com>
2021-10-26 22:46:33 -0400

    commit tree 95f46f1639991a2cb23c257d35588e2ac4ec7039

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const findMatching = (drivers, x) => {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === x.toLowerCase();
+  });
+};
+
+const fuzzyMatch = (drivers, x) => {
+  return drivers.filter(function (driver) {
+    return driver.startsWith(x);
+  });
+};
+
+const matchName = (drivers, x) => {
+  return drivers.filter(function (driver) {
+    return driver.name === x;
+  });
+};
